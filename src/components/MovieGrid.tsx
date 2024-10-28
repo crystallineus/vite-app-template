e80694a1176a9c14178d77a1770e04d4340fbac6
Chien-Shan Peng <crystalline@crystalline.work>
2024-10-27 22:25:15 -0700

    refactor: add movie card

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Movie } from "../models/Movie";
+import MovieCard from "./MovieCard";
 
 function MovieGrid() {
     const [movies, setMovies] = useState<Movie[]>([]);
@@ -12,14 +13,7 @@ function MovieGrid() {
         <div className="movies-grid">
             {
                 movies.map(movie => (
-                    <div key={movie.id} className="movie-card">
-                        <img src={`images/${movie.image}`} alt={movie.title}/>
-                        <div className="movie-card-info">
-                            <h3 className="movie-card-title">{movie.title}</h3>
-                            <p className="movie-card-genre">{movie.genre}</p>
-                            <p className="movie-card-rating">{movie.rating}</p>
-                        </div>
-                    </div>
+                    <MovieCard movie={movie} key={movie.id} />
                 ))
             }
         </div>
