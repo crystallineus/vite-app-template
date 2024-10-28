7d86720ef3c5569161b4dbfcca6ab223cb67a4a7
Chien-Shan Peng <crystalline@crystalline.work>
2024-10-27 22:29:55 -0700

    add imgae error handler

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,9 +1,13 @@
 import { Movie } from "../models/Movie";
 
 function MovieCard({movie} : {movie: Movie}) {
+    const handleError = (e: any) => {
+        e.target.src = "images/default.jpg";
+    };
+
     return (
         <div key={movie.id} className="movie-card">
-            <img src={`images/${movie.image}`} alt={movie.title}/>
+            <img src={`images/${movie.image}`} alt={movie.title} onError={handleError}/>
             <div className="movie-card-info">
                 <h3 className="movie-card-title">{movie.title}</h3>
                 <p className="movie-card-genre">{movie.genre}</p>
